ee00304d99a3b0d59a11492993d2553640fd0166
siri <madhurisireesha6@gmail.com>
2023-11-06 16:27:54 +0530

    Trendz

---
+++ src/components/Home/index.js
@@ -0,0 +1,36 @@
+import './index.css'
+
+const Home = props => {
+  // eslint-disable-next-line
+  
+  return (
+    <>
+     
+      {/* <div className="home-container"> */}
+        <div className="c1">
+          <div className="c2">
+            <h1 className="heading">Clothes That Get YOU Noticed</h1>
+            <p className="para">
+              Fashion is part of the daily air and it does not quite help that
+              it changes all the time.Clothes have analysis been a market of the
+              era and we are import from a revolution . Fashion is part of the
+              daily air and it does not quite help that it changes all the
+              time.Clothes have analysis been a market of the era and we are
+              import from a revolution
+            </p>
+            <button className="btn3" type="button">
+              Shop Now
+            </button>
+        </div>
+            <div>
+             <img
+             src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+                className="img34"
+                alt="clothes that get you noticed"
+          /></div>
+        </div>
+      {/* </div> */}
+    </>
+  )
+}
+export default Home
